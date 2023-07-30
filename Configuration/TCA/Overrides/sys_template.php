3c0337ff73f6e5ef214ec0208551ae3437b40a02
Sven Wappler <info@wappler.systems>
2023-07-31 00:40:50 +0200

    update

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -2,6 +2,5 @@
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-defined('TYPO3_MODE') or die();
 
 ExtensionManagementUtility::addStaticFile('pretix', 'Configuration/TypoScript/', 'Pretix Template');
--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,7 +3,6 @@
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
-defined('TYPO3') || die();
 
 ExtensionUtility::registerPlugin(
     'pretix',
